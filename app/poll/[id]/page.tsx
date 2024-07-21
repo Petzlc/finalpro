d17062b23d3438c8897a7a6fc0ba8eedd55b69ce
Petzlc <chlodwig.petzl@hotmail.com>
2024-07-21 13:10:53 +0100

    Add link to poll results to pollpage.tsx

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -1,5 +1,9 @@
+import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { getOptionsByPollIdInsecure } from '../../../database/options';
 import { getPollByIdInsecure } from '../../../database/polls';
+import { getResponseByPollIdAndUserIdInsecure } from '../../../database/responses';
+import { getValidSession } from '../../../database/sessions';
 import PollOptions from '../../components/PollOptions';
 
 // import PollResults from '../../components/PollResults';
@@ -17,12 +21,39 @@ export default async function PollPage({ params }: Props) {
   if (!poll) {
     return <div>Poll not found</div>;
   }
+
+  // make sure every User only takes poll once
+  // 1. Get the session token from cookies
+  const cookieStore = cookies();
+  const sessionToken = cookieStore.get('sessionToken')?.value;
+
+  let userHasResponded = false;
+
+  if (sessionToken) {
+    // 2. Validate the session and get the user ID
+    const session = await getValidSession(sessionToken);
+
+    if (session && session.userId) {
+      // 3. Check if the user has already responded to this poll
+      const existingResponse = await getResponseByPollIdAndUserIdInsecure(
+        pollId,
+        session.userId,
+      );
+      userHasResponded = !!existingResponse;
+    }
+  }
+
   return (
     <div>
       <h1>{poll.title}</h1>
       <p>{poll.description}</p>
-      <PollOptions pollId={pollId} options={options} />
+      {userHasResponded ? (
+        <div>You have already responded to this poll</div>
+      ) : (
+        <PollOptions pollId={pollId} options={options} />
+      )}
       {/* <PollResults pollId={pollId} /> */}
+      <Link href={`/results?pollId=${pollId}`}>View Results</Link>
     </div>
   );
 }
