02a3e8df16e1b6e5e172a77c46c9bb9572b0da65
Petzlc <chlodwig.petzl@hotmail.com>
2024-07-22 11:28:39 +0100

    Add style to the landing page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { getRandomPollsInsecure } from '../database/polls';
+import styles from './page.module.scss';
 
 export const metadata = {
   title: 'Home | Poll App',
@@ -10,19 +11,23 @@ export default async function Home() {
   // Generate 3 random polls on the main page
   const polls = await getRandomPollsInsecure(3);
   return (
-    <div>
-      <div>
+    <div className={styles.homeContainer}>
+      <div className={styles.heroText}>
         <h1>Raise your hands for destiny</h1>
       </div>
-      <div>
-        <Link href="/register">Sign Up</Link>
+      <div className={styles.linkContainer}>
+        <Link href="/register" className={styles.styledLink}>
+          Sign Up
+        </Link>
       </div>
-      <div>
-        <Link href="/createPolls">Take a Poll</Link>
+      <div className={styles.linkContainer}>
+        <Link href="/createPolls" className={styles.styledLink}>
+          Take a Poll
+        </Link>
       </div>
-      <div>
+      <div className={styles.pollCardsContainer}>
         {polls.map((poll) => (
-          <div key={`poll-${poll.id}`}>
+          <div key={`poll-${poll.id}`} className={styles.pollCard}>
             <Link href={`/poll/${poll.id}`}>
               <h3>{poll.title}</h3>
               <p>{poll.description}</p>
